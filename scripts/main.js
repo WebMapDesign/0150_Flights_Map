fcd1cccfb012f308581600dd3e858b96c2065650
WebMapDesign <101799669+WebMapDesign@users.noreply.github.com>
2022-03-26 01:29:12 +0200

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,58 @@
+// airport details
+let airportDepartureName = "Siegerland";
+let airportDepartureCode = "EDGS";
+let airportDestinationName = "Berlin";
+let airportDestinationCode = "EDAZ";
+let airplaneModel = "Cessna C152";
+let airplaneHourlyRate = 286; // EUR
+let airplaneSpeedKt = 109; //kt
+let airplaneSpeedKmH = airplaneSpeedKt * 1.852; // km.h
+
+let pilotName = "Robert Anders";
+let pilotHourlyRate = 100; // EUR
+let routeDistance = 300; // kilometers
+let routeDuration = 100; // minutes
+
+// select containers
+const spanAirportDepartureName = document.getElementById("airport-dep");
+const spanairportDestinationName = document.getElementById("airport-dest");
+const spanAirportDepartureCode = document.getElementById("airport-dep-code");
+const spanAirportDestinationCode = document.getElementById("airport-dest-code");
+const spanAirplaneModel = document.getElementById("airplane-model");
+const spanPilotName = document.getElementById("pilot-name");
+const spanRouteDistance = document.getElementById("route-distance");
+const spanRouteDuration = document.getElementById("route-duration");
+const spanAirplaneRate = document.getElementById("airplane-rate");
+const spanPilotRate = document.getElementById("pilot-rate");
+const spanTotalRate = document.getElementById("total-rate");
+
+// assign values
+spanAirportDepartureName.innerText = airportDepartureName;
+spanAirportDepartureCode.innerText = "(" + airportDepartureCode + ")";
+spanairportDestinationName.innerText = airportDestinationName;
+spanAirportDestinationCode.innerText = "(" + airportDestinationCode + ")";
+spanAirplaneModel.innerText = airplaneModel;
+spanPilotName.innerText = pilotName;
+spanRouteDistance.innerText = routeDistance + " km";
+spanRouteDuration.innerText = routeDuration + " minutes";
+spanAirplaneRate.innerText = airplaneHourlyRate + " EUR/hour";
+spanPilotRate.innerText = pilotHourlyRate + " EUR/hour";
+spanTotalRate.innerText =
+  ((airplaneHourlyRate + pilotHourlyRate) * routeDuration) / 60 + " EUR/hour";
+
+function selectDeparture() {
+  console.log(this);
+  airportDepartureName = "Departure Selected";
+  spanAirportDepartureName.innerText = airportDepartureName;
+}
+
+function selectDestination() {
+  airportDestinationName = "Destination Selected";
+  spanairportDestinationName.innerText = airportDestinationName;
+}
+
 const urlGoogleSheetAirports =
-  "https://docs.google.com/spreadsheets/d/e/2PACX-1vTrlVbYsVAROf6X9fqj8E_2oNqUueDDwJtgG9eBBFrQBDiMAir1CbZ3UOs24kaN13CLsIHRWrQ3pv-Q/pub?gid=0&single=true&output=csv";
+  "https://docs.google.com/spreadsheets/d/e/2PACX-1vSfIDiI9p53KnEUi7lo5hXXhxF6_euLss0MuYpeezjJ7lYvFJDbst7U8FqrKfZwGG8qZ_aqHtVCvf81/pub?output=csv";
 
 let geojsonAirports = {
   type: "FeatureCollection",
@@ -23,15 +76,15 @@ function getAirportData() {
         let newAirport = {
           type: "Feature",
           properties: {
-            airport_id: i["Airport ID"],
-            airport_name: i["Name"],
-            airport_city: i["City"],
-            airport_country: i["Country"],
-            airport_code: i["IATA Code"],
-            destination_id_1: i["Destination ID 1"],
-            destination_id_2: i["Destination ID 2"],
-            destination_id_3: i["Destination ID 3"],
-            destination_id_4: i["Destination ID 4"],
+            airport_id: i["ID"],
+            airport_code: i["Airfield"],
+            airport_city: i["Location"],
+            // airport_name: i["Name"],
+            // airport_country: i["Country"],
+            // destination_id_1: i["Destination ID 1"],
+            // destination_id_2: i["Destination ID 2"],
+            // destination_id_3: i["Destination ID 3"],
+            // destination_id_4: i["Destination ID 4"],
           },
           geometry: {
             type: "Point",
@@ -53,13 +106,13 @@ function getAirportData() {
 getAirportData();
 
 function drawMap() {
-  let startLat = 48.52;
-  let startLong = 13.83;
-  let startZoom = 4;
+  let startLat = 51.8;
+  let startLong = 13.0;
+  let startZoom = 6;
 
   let map = L.map("map", {
     fullScreenControl: false,
-    zoomSnap: 1,
+    zoomSnap: 0.5,
     minZoom: 4,
     // maxZoom: 10,
     // maxBounds: mapBounds,
@@ -76,6 +129,14 @@ function drawMap() {
     },
     "Default View"
   ).addTo(map);
+
+  let sidebarControlMenu = L.control.sidebar("sidebar-results", {
+    position: "right",
+    closeButton: false,
+    autoPan: false,
+  });
+  map.addControl(sidebarControlMenu);
+  sidebarControlMenu.show();
 
   let tiles_OSM = L.tileLayer(
     "https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw",
@@ -143,26 +204,38 @@ function drawMap() {
   }
 
   function onEachFeatureAirports(feature, layer) {
-    let airportName = feature.properties.airport_name;
-    let airportCity = feature.properties.airport_city;
-    let airportCountry = feature.properties.airport_country;
     let airportCode = feature.properties.airport_code;
+    let airportCity = feature.properties.airport_city;
+    // let airportName = feature.properties.airport_name;
+    // let airportCountry = feature.properties.airport_country;
 
-    let popupContent = '<p class="popup-title">' + airportName + "</p>";
+    let popupContent = '<p class="popup-title">' + airportCode + "</p>";
 
     if (airportCode) {
-      popupContent += '<p class="popup-subtitle">(' + airportCode + ")</p>";
+      popupContent += '<p class="popup-subtitle">' + airportCity + "</p>";
     }
 
-    if (airportCity && airportCountry) {
-      popupContent +=
-        '<p class="popup-text">' + airportCity + ", " + airportCountry + "</p>";
-    }
+    // if (airportCity && airportCountry) {
+    //   popupContent +=
+    //     '<p class="popup-text">' + airportCity + ", " + airportCountry + "</p>";
+    // }
+
+    // popupContent +=
+    //   '<button type="button" class="btn-select-airport btn-departure" onclick="selectDeparture()">' +
+    //   "Select as Departure" +
+    //   "</button>";
+
+    // popupContent += "</br>";
+
+    // popupContent +=
+    //   '<button type="button" class="btn-select-airport btn-destination" onclick="selectDestination()">' +
+    //   "Select as Destination" +
+    //   "</button>";
 
     layer.bindPopup(popupContent, {});
 
     layer.on({
-      click: drawAvailableRoutes,
+      // click: drawAvailableRoutes,
     });
   }
 
@@ -173,43 +246,51 @@ function drawMap() {
     onEachFeature: onEachFeatureAirports,
   }).addTo(map);
 
-  var center = [11.09957366, 60.19393548];
-  let radius1 = 1000;
-  let radius2 = 2000;
-  let radius3 = 3000;
+  let pointA = turf.point([8.076225, 50.707205]); // EDGS Siegerland
+  let pointB = turf.point([13.154605, 52.207172]); // EDAZ Berlin
+  let routeGeojson = turf.greatCircle(pointA, pointB);
+  let route = L.geoJSON(routeGeojson, {
+    style: styleRouteLines,
+  }).addTo(map);
 
-  let options = {
+  let radiusPlane = 678;
+
+  let optionsCircle = {
     steps: 200,
     units: "kilometers",
-    properties: { foo: "bar" },
   };
 
-  let circle1 = turf.circle(center, radius1, options);
-  let circle2 = turf.circle(center, radius2, options);
-  let circle3 = turf.circle(center, radius3, options);
+  let circleRange = turf.circle(pointA, radiusPlane, optionsCircle);
 
-  let layerRange1 = L.geoJSON(circle1, {
+  let layerPlaneRange = L.geoJSON(circleRange, {
     style: { fillOpacity: 0, color: "#000000", weight: 1 },
   }).addTo(map);
 
-  let layerRange2 = L.geoJSON(circle2, {
-    style: { fillOpacity: 0, color: "#000000", weight: 1 },
-  }).addTo(map);
-
-  let layerRange3 = L.geoJSON(circle3, {
-    style: { fillOpacity: 0, color: "#000000", weight: 1 },
-  }).addTo(map);
-
-  let baseLayers = {};
-
-  let overlays = {
-    Airports: layerAirports,
-    "Range 1000km": layerRange1,
-    "Range 2000km": layerRange2,
-    "Range 3000km": layerRange3,
+  let optionsDistance = {
+    units: "kilometers",
   };
 
-  L.control
-    .layers(baseLayers, overlays, { collapsed: false, position: "topright" })
-    .addTo(map);
+  routeDistance = turf.distance(pointA, pointB, optionsDistance);
+  spanRouteDistance.innerText = routeDistance.toFixed(1) + " km";
+
+  routeDuration = (routeDistance / airplaneSpeedKmH) * 60;
+  spanRouteDuration.innerText = routeDuration.toFixed(0) + " minutes";
+  console.log(routeDuration);
+
+  spanTotalRate.innerText =
+    (((airplaneHourlyRate + pilotHourlyRate) * routeDuration) / 60).toFixed(0) +
+    " EUR/hour";
+
+
+    let iconArrow = L.icon({
+      iconUrl: "images/arrow.jpeg",
+      iconSize: [25, 25],
+    });
+
+  let midpoint = turf.midpoint(pointA, pointB);
+  let lyrMidpoint = L.geoJSON(midpoint, {
+    pointToLayer: function (feature, latlng) {
+      return L.marker(latlng, { icon: iconArrow });
+    },
+  }).addTo(map);
 }
